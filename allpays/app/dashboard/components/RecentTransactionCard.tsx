cd5bc39ce7615707d10d0bb41d83109d21af5155
ddhsl <dnjs9199@naver.com>
2025-12-03 21:36:33 +0900

    feat: recent transaction api 연결

--- allpays/app/dashboard/components/RecentTransactionCard.tsx
+++ allpays/app/dashboard/components/RecentTransactionCard.tsx
@@ -1,20 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
 import TransactionsTable from "@/app/transactions/components/TransactionsTable";
-import { Transaction } from "@/app/transactions/page";
+import { getTransactions } from "@/api/transactions/transactions";
+import { getMerchants } from "@/api/merchants/merchants";
+import { attachStoreNames } from "@/utils/calcTopStores";
+import { Transaction } from "@/hooks/useTransactions";
+import { mapTransactions } from "@/utils/mapTransactions";
 
-interface RecentTransactionsCardProps {
-  transactions: Transaction[];
-}
+export default function RecentTransactionsCard() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-export default function RecentTransactionsCard({
-  transactions,
-}: RecentTransactionsCardProps) {
-  const recent5 = transactions.slice(0, 5);
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const { data: txData } = await getTransactions();
+        const { data: storesData } = await getMerchants();
+
+        const recent5 = txData.data.slice(0, 5);
+
+        const top5WithNames = attachStoreNames(
+          recent5.map((tx) => ({
+            mchtCode: tx.mchtCode,
+            totalAmount: Number(tx.amount),
+          })),
+          storesData.data
+        );
+
+        const tableData = mapTransactions(recent5, storesData.data).map((t) => {
+          const store = top5WithNames.find((s) => s.mchtCode === t.code);
+          return {
+            ...t,
+            merchant: store?.mchtName || t.merchant,
+          };
+        });
+
+        setTransactions(tableData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
 
   return (
-    <article className="flex-1 rounded-xl shadow-sm flex flex-col p-6 bg-white border border-[#cbd5e1]">
+    <article className="flex-1 rounded-xl shadow-md flex flex-col p-6 bg-white/80 backdrop-blur-sm border border-[#E5E7EB]">
       <h3 className="text-lg font-medium text-secondary mb-4">최근 거래 5건</h3>
       <div className="overflow-x-auto min-w-[600px]">
-        <TransactionsTable transactions={recent5} />
+        <TransactionsTable transactions={transactions} />
       </div>
     </article>
   );
